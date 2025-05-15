701b9c4e0a7059701b41667efeafdd06a73676bc
Vasie <59412971+Vasie1337@users.noreply.github.com>
2025-05-15 10:15:21 +0200

    Remove

--- bpvmatcher/bpvmatcher/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('stages.urls')), # 'stages' is de naam van je app
-]
